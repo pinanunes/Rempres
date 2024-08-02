d337d0eabe598f2c7a850f0beb16f7f1e16eca89
Telmo Nunes <pinanunes@gmail.com>
2024-08-02 15:20:51 +0100

    added data

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Disease List Dataset
+#'
+#' A dataset containing disease names alias.
+#'
+#' @format A data frame with 123 rows (diseases) and 5 variables:
+#' \describe{
+#'   \item{disease_name}{Name of the disease as is mentioned on Empres-i reports}
+#'   \item{acronym}{English acronym for the disease}
+#'   \item{portuguese_name}{The official portuguese name for the disease}
+#'   \item{common_english_name}{Common English name for the disease}
+#'   \item{portuguese_aconym}{Portuguese acronym for the disease}
+#'   \item{link_EFSA}{Link for EFSA's page for the disease}
+#'   \item{url}{link to WOAH's page for the disease}
+#' }
+#' @source \url{https://www.woah.org/}
+"disease_list2"
